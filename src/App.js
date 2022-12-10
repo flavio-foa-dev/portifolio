e3fd435d77ae3c0e567b9b13fb7c947e61e97b8e
Flavio Andrade <foa12381@gmail.com>
2022-12-10 13:25:49 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom'
+
 import './App.css';
+import Home from './pages/home';
+import Perfil from './pages/perfil';
+import Project from './pages/project';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Flavio Andrade <code>src/App.js</code> initial services
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route path="/perfil" component={Perfil}></Route>
+        <Route path="/project" component={Project}></Route>
+      </Switch>
     </div>
   );
 }
