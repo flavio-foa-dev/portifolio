b74845b86a0dfe813cd1a02789f520694b520cf2
Flavio Andrade <foa12381@gmail.com>
2022-12-15 11:48:48 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import React from 'react';
+import {Routes, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/home';
+import Perfil from './pages/perfil';
+import Project from './pages/project';
 
 function App() {
   return (
     <div className="App">
-      <h1>Home</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/perfil" element={<Perfil/>} />
+        <Route path="/project" element={<Project/>} />
+      </Routes>
     </div>
   );
 }
