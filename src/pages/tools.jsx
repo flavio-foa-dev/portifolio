fe50e577da3949bb76a52780c5e6e2f90573fba2
Flavio Andrade <foa12381@gmail.com>
2022-12-17 12:30:31 -0300

    feat/pages/project

--- src/pages/tools.jsx
+++ src/pages/tools.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Footer from '../components/footer'
-import Header from '../components/navBar'
-import Project from './project'
 
 export default function Tools() {
   return (
